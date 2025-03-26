38cb5d09e43f89e7bea9586543f7b0595d8a997f
BigTitouDev <160507100+BigTitouDev@users.noreply.github.com>
2025-03-26 18:51:29 +0100

    Add files via upload

--- Code/Clustering.R
+++ Code/Clustering.R
@@ -5,17 +5,17 @@ library(cluster)
 
 
 # Importation des données (issue de la data preparation)
-data_maladie_effectifs_finistere <- read.csv("Data/data_maladie_effectifs_finistere.csv", sep = ",")
+data_maladie_preparation <- read.csv("data_maladie_preparation.csv", sep = ",")
 
 ## Mise en forme du jeu de données pour l'application du clustering ##
 
 # On pivote le dataset de sorte à avoir une instance par pathologie associée à une prevalence et un nombre de cas pour chaque année
-data_maladie_effectifs_finistere <- reshape(data_maladie_effectifs_finistere, idvar = 'patho_niv2', timevar = 'annee', direction = 'wide')
+data_clustering <- reshape(data_maladie_preparation, idvar = 'patho_niv2', timevar = 'annee', direction = 'wide')
 
 # Réinitialiser l'index
-rownames(data_maladie_effectifs_finistere) <- 1:nrow(data_maladie_effectifs_finistere)
+rownames(data_clustering) <- 1:nrow(data_clustering)
 
-data_maladie_effectifs_finistere <- na.omit(data_maladie_effectifs_finistere)
+data_clustering <- na.omit(data_clustering)
 
 # Afin d'analyser les variation de la prévalence des pathologies, on calcul le taux de variation de la prévalence pour chaque année (ex : le taux d'augmentation entre 2017 et 2018)
 for (year in 2016:2022) {
@@ -23,28 +23,26 @@ for (year in 2016:2022) {
   prev_previous <- paste0('prev.', year - 1)
   taux_column <- paste0('taux.', year)
   
-  data_maladie_effectifs_finistere[[taux_column]] <- (data_maladie_effectifs_finistere[[prev_current]] - data_maladie_effectifs_finistere[[prev_previous]]) / data_maladie_effectifs_finistere[[prev_previous]] * 100  # En pourcentage
+  data_clustering[[taux_column]] <- (data_clustering[[prev_current]] - data_clustering[[prev_previous]]) / data_clustering[[prev_previous]] * 100  # En pourcentage
 }
 
 
 # On va s'interesser à la variation moyenne du taux sur ces 5 dernieres années, on calcul donc la moyenne des taux de variation pour chaque pathologie et la moyenne du nombre de cas
-data_maladie_effectifs_finistere$moyenne_cas <- rowMeans(data_maladie_effectifs_finistere[, c('ntop.2015', 'ntop.2016','ntop.2017', 'ntop.2018', 'ntop.2019', 'ntop.2020', 'ntop.2021', 'ntop.2022')], na.rm = TRUE)
+data_clustering$moyenne_cas <- rowMeans(data_clustering[, c('ntop.2015', 'ntop.2016','ntop.2017', 'ntop.2018', 'ntop.2019', 'ntop.2020', 'ntop.2021', 'ntop.2022')], na.rm = TRUE)
 
-data_maladie_effectifs_finistere$moyenne_taux <- rowMeans(data_maladie_effectifs_finistere[, c('taux.2016','taux.2017', 'taux.2018', 'taux.2019', 'taux.2020', 'taux.2021', 'taux.2022')], na.rm = TRUE)
+data_clustering$moyenne_taux <- rowMeans(data_clustering[, c('taux.2016','taux.2017', 'taux.2018', 'taux.2019', 'taux.2020', 'taux.2021', 'taux.2022')], na.rm = TRUE)
 
 # Suppression des colonnes inutiles pour le clustering
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[, c('patho_niv2', 'moyenne_cas', 'moyenne_taux')]
+data_clustering <- data_clustering[, c('patho_niv2', 'moyenne_cas', 'moyenne_taux')]
 
 # Normalisation des données
-data_maladie_effectifs_finistere[, c('moyenne_cas', 'moyenne_taux')] <- scale(data_maladie_effectifs_finistere[, c('moyenne_cas', 'moyenne_taux')])
+data_clustering[, c('moyenne_cas', 'moyenne_taux')] <- scale(data_clustering[, c('moyenne_cas', 'moyenne_taux')])
 
 
 ## Clustering ##
 
 
 set.seed(123)
-
-data_clustering <- data_maladie_effectifs_finistere
 
 # Sélection des colonnes pour le clustering
 X <- data_clustering[, c('moyenne_cas', 'moyenne_taux')]
@@ -89,4 +87,4 @@ data_clustering %>%
     moyenne_taux = mean(moyenne_taux)  # Moyenne des taux
   ) %>% arrange(-moyenne_taux)
 
-write.csv(data_clustering, "Data/data_clustering.csv", row.names = FALSE)
+write.csv(data_clustering, "data_clustering.csv", row.names = FALSE)
