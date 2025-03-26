3aec8f7a0634d60e1b917099b63477d436e762ef
BigTitouDev <160507100+BigTitouDev@users.noreply.github.com>
2025-03-26 18:44:48 +0100

    Add files via upload

--- Code/Data_preparation.R
+++ Code/Data_preparation.R
@@ -1,31 +1,30 @@
 
 # Importation des données
-data_maladie_effectifs <- read.csv("effectifs.csv", sep = ";")
+data_maladie_effectifs <- read.csv("Data/data_maladie_effectifs_finistere.csv", sep = ",")
 
 # Selection des données des pathologies du finistere pour age et sexe confondu
 
-data_maladie_effectifs_finistere <- data_maladie_effectifs[data_maladie_effectifs$dept == '29', ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$annee >= 2015 & data_maladie_effectifs_finistere$annee <= 2023, ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$cla_age_5 == 'tsage', ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$sexe == 9, ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[!is.na(data_maladie_effectifs_finistere$patho_niv2), ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[, c('annee', 'patho_niv2', 'ntop', 'npop')]
+data_maladie_preparation <- data_maladie_effectifs[data_maladie_effectifs$annee >= 2015 & data_maladie_effectifs$annee <= 2023, ]
+data_maladie_preparation <- data_maladie_preparation[data_maladie_preparation$cla_age_5 == 'tsage', ]
+data_maladie_preparation <- data_maladie_preparation[data_maladie_preparation$sexe == 9, ]
+data_maladie_preparation <- data_maladie_preparation[!is.na(data_maladie_preparation$patho_niv2), ]
+data_maladie_preparation <- data_maladie_preparation[, c('annee', 'patho_niv2', 'ntop', 'npop')]
 
 # Supprimer les valeurs des pathologies non intéressantes ou qui peuvent induire des biais dans les modèles :
 
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$patho_niv2 != 'Total consommants tous régimes', ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$patho_niv2 != 'Pas de pathologie repérée, traitement, maternité, hospitalisation ou traitement antalgique ou anti-inflammatoire', ]
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[data_maladie_effectifs_finistere$patho_niv2 != '', ]
+data_maladie_preparation <- data_maladie_preparation[data_maladie_preparation$patho_niv2 != 'Total consommants tous régimes', ]
+data_maladie_preparation <- data_maladie_preparation[data_maladie_preparation$patho_niv2 != 'Pas de pathologie repérée, traitement, maternité, hospitalisation ou traitement antalgique ou anti-inflammatoire', ]
+data_maladie_preparation <- data_maladie_preparation[data_maladie_preparation$patho_niv2 != '', ]
 
 
 # On regroupe les données par année et par pathologie et on fait la somme des cas et de la population concernée
-data_maladie_effectifs_finistere <- aggregate(cbind(ntop, npop) ~ annee + patho_niv2, data = data_maladie_effectifs_finistere, mean)
+data_maladie_preparation <- aggregate(cbind(ntop, npop) ~ annee + patho_niv2, data = data_maladie_preparation, mean)
 
 # On calcul un taux de prévalence pour chaque pathologie (nombre de cas / population concernée) et années
-data_maladie_effectifs_finistere$prev <- data_maladie_effectifs_finistere$ntop / data_maladie_effectifs_finistere$npop
+data_maladie_preparation$prev <- data_maladie_preparation$ntop / data_maladie_preparation$npop
 
 # On supprime la colonne de la population concernée
-data_maladie_effectifs_finistere <- data_maladie_effectifs_finistere[, c('annee', 'patho_niv2','ntop', 'npop', 'prev')]
+data_maladie_preparation <- data_maladie_preparation[, c('annee', 'patho_niv2','ntop', 'npop', 'prev')]
 
 
-write.csv(data_maladie_effectifs_finistere, "data_maladie_effectifs_finistere.csv", row.names = TRUE)
+write.csv(data_maladie_preparation, "Data/data_maladie_preparation.csv", row.names = FALSE)
